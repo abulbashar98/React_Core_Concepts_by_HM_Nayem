2c7abf5f780b5dd86dd8c83f6cd53af5e5164c02
abulbashar98 <abulbashar98116@gmail.com>
2022-07-21 13:24:48 +0600

    chapter_three completed

---
+++ first-react-app/src/Components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello React, Now Working with index.js path correction</h1>
+    }
+}
+
+export default App;
+
