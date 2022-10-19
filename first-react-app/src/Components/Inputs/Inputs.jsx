4066015cc450d72aee18b130854db542d7e4fcf5
abulbashar98 <abulbashar98116@gmail.com>
2022-10-19 18:54:04 +0600

    Radio buttons, and checkbox es , In groups, Checkbox and working with Skill AArray

--- first-react-app/src/Components/Inputs/Inputs.jsx
+++ first-react-app/src/Components/Inputs/Inputs.jsx
@@ -8,7 +8,10 @@ class App extends React.Component {
         name: '',
         country: '',
         bio: '',
-        birthDate: ''
+        birthDate: '',
+        gender: '',
+        agree: false,
+        skills: []
     }
 
     handleChange = event => {
@@ -19,8 +22,41 @@ class App extends React.Component {
         )
     }
 
+    handleCheckBox = event => {
+        this.setState(
+            {
+                agree: event.target.checked
+            }
+        )
+    }
+
+    handleSkillChange = event => {
+
+        if (event.target.checked) {
+            this.setState(
+                {
+                    skills: [...this.state.skills, event.target.value]
+                }
+            )
+        }
+        else {
+            const skills = this.state.skills.filter(skill => skill !== event.target.value)
+            this.setState(
+                {
+                    skills: [...skills]
+                }
+            )
+        }
+
+
+    }
+
+
+
 
     render() {
+
+        const { name, country, bio, birthDate, agree } = this.state;
 
         return (
 
@@ -28,6 +64,7 @@ class App extends React.Component {
                 <input
                     type="text"
                     name='name'
+                    value={name}
                     placeholder='AB'
                     className='form-control my-5'
                     onChange={this.handleChange}
@@ -35,6 +72,7 @@ class App extends React.Component {
                 <select
                     name='country'
                     className='form-control my-5'
+                    value={country}
                     onChange={this.handleChange}
                 >
                     <option>Select One</option>
@@ -49,6 +87,7 @@ class App extends React.Component {
                     id=""
                     cols="30"
                     rows="10"
+                    value={bio}
                     placeholder='tell us about yourself'
                     className='form-control my-5'
                     onChange={this.handleChange}
@@ -58,11 +97,93 @@ class App extends React.Component {
                     type="date"
                     name='birthDate'
                     className='form-control my-5'
+                    value={birthDate}
                     onChange={this.handleChange}
                 />
+
+                <div>
+                    Gender:
+                    <br />+-
+                    k 9
+                    <input
+                        type="radio"
+                        name='gender'
+                        value="Male"
+                        onChange={this.handleChange}
+                    />
+                    Male
+                    <input
+                        type="radio"
+                        name='gender'
+                        value="Female"
+                        onChange={this.handleChange}
+                    />
+                    Female
+                    <input
+                        type="radio"
+                        name='gender'
+                        value="Other"
+                        onChange={this.handleChange}
+                    />
+                    Other
+                </div>
+                <div>
+                    Skills: <br />
+                    <input
+                        type="checkbox"
+                        name='Skills'
+                        value="Python"
+                        checked={this.state.skills.includes("Python")}
+                        onChange={this.handleSkillChange}
+                    />
+                    Python
+                    <input
+                        type="checkbox"
+                        name='Skills'
+                        value="Javascript"
+                        checked={this.state.skills.includes("Javascript")}
+                        onChange={this.handleSkillChange}
+                    />
+                    Javascript
+                    <input
+                        type="checkbox"
+                        name='Skills'
+                        value="C#"
+                        checked={this.state.skills.includes("C#")}
+                        onChange={this.handleSkillChange}
+                    />
+                    C#
+                    <input
+                        type="checkbox"
+                        name='Skills'
+                        value=".Net"
+                        checked={this.state.skills.includes(".Net")}
+                        onChange={this.handleSkillChange}
+                    />
+                    .Net
+                    <input
+                        type="checkbox"
+                        name='Skills'
+                        value="Java"
+                        checked={this.state.skills.includes("Java")}
+                        onChange={this.handleSkillChange}
+                    />
+                    Java
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        name='agree'
+                        checked={agree}
+                        onChange={this.handleCheckBox}
+                    />
+                    Yes, I agree to all the terms and Conditions.
+                </div>
+
                 <button
                     className='btn btn-danger'
                     onClick={() => console.log(this.state)}
+                // onClick={() => alert(this.state.name)}
                 >
                     Show Changes
                 </button>
@@ -76,4 +197,4 @@ class App extends React.Component {
 
 }
 
-export default App;
+export default App; 
--- first-react-app/src/Components/app.jsx
+++ first-react-app/src/Components/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Inputs from './Inputs/Inputs'
 
 class App extends React.Component {
