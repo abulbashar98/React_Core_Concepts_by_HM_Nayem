b12a78f46d8d3d8ee490cdaa6a5a1d68e6a2d332
abulbashar98 <abulbashar98116@gmail.com>
2022-11-14 18:48:08 +0600

    Squares and Board Set up, with Index Logic and State lifting Logic

--- tic-tac-toe/src/components/game.jsx
+++ tic-tac-toe/src/components/game.jsx
@@ -1,14 +1,33 @@
 
 import React from "react";
+import Board from "./board";
 
 
 
 class Game extends React.Component {
 
+    state = {
+
+        history: [{ squares: Array(9).fill(null) }]
+
+    }
+
+    onClick = i => {
+        console.log(i)
+    }
+
+
+
     render() {
 
         return (
-            <div><h2>Game Component</h2></div>
+            <div>
+                <h2>Game Component</h2>
+                <Board
+                    squares={this.state.history[0]}
+                    onClick={this.onClick}
+                />
+            </div>
         )
 
 
