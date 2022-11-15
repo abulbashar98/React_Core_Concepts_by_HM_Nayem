5b4a9188c1d84ae5a0fd0557c75449a6d7b429af
abulbashar98 <abulbashar98116@gmail.com>
2022-11-15 19:24:32 +0600

    Game Winning Logic Implemented

--- tic-tac-toe/src/components/game.jsx
+++ tic-tac-toe/src/components/game.jsx
@@ -4,7 +4,30 @@ import Board from "./board";
 
 
 function calculateWinner(squares) {
-    return
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+
+        const [a, b, c] = lines[i];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+
+    }
+
+    return null
+
+
 }
 
 
@@ -17,7 +40,7 @@ class Game extends React.Component {
     }
 
     handleClick = i => {
-        const history = this.state.history.slice(0, (this.stepCount + 1))
+        const history = this.state.history.slice(0, (this.state.stepCount + 1))
         const current = history[history.length - 1]
         const squares = current.squares.slice()
 
@@ -38,11 +61,18 @@ class Game extends React.Component {
 
     render() {
 
+        const history = this.state.history;
+        const current = history[this.state.stepCount]
+        const winner = calculateWinner(current.squares)
+        console.log(winner)
+
+
+
         return (
             <div>
                 <h2>Game Component</h2>
                 <Board
-                    squares={this.state.history[0]}
+                    squares={current.squares}
                     onClick={this.handleClick}
                 />
             </div>
